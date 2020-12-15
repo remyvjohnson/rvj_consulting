1f9516a4d07447928a7aabaee99c194904574673
Remy Johnson <remyvjohnson@gmail.com>
2020-12-15 13:32:09 -0800

    Adding files for bash script

---
+++ build.sh
@@ -0,0 +1,4 @@
+cat templates/top.html content/index_content.html templates/bottom.html > docs/index.html
+cat templates/top.html content/services_content.html templates/bottom.html > docs/services.html
+cat templates/top.html content/about_content.html templates/bottom.html > docs/about.html
+cat templates/top.html content/contact_content.html templates/bottom.html > docs/contact.html
