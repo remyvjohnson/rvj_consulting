0f147551bc3bd17986709997ecb4d83ff3393755
Remy Johnson <remyvjohnson@gmail.com>
2020-12-15 14:36:49 -0800

    Remainder of Python SSG

--- build.py
+++ build.py
@@ -10,3 +10,20 @@ content = open('content/index_content.html').read()
 index_html = top_template + content + bottom_template
 open('docs/index.html', 'w+').write(index_html)
 
+# Repeat for Services file
+content = open('content/services_content.html').read()
+
+services_html = top_template + content + bottom_template
+open('docs/services.html', 'w+').write(services_html)
+
+# Repeat for About file
+content = open('content/about_content.html').read()
+
+about_html = top_template + content + bottom_template
+open('docs/about.html', 'w+').write(about_html)
+
+# Repeat for Contact file
+content = open('content/contact_content.html').read()
+
+contact_html = top_template + content + bottom_template
+open('docs/contact.html', 'w+').write(contact_html)
