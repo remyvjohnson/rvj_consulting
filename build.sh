a6652b0d08c01008919a5a2baef37830d9553259
Remy Johnson <remyvjohnson@gmail.com>
2020-12-18 12:44:35 -0800

    deleted build.sh file

--- build.sh
+++
@@ -1,4 +0,0 @@
-cat templates/top.html content/index_content.html templates/bottom.html > docs/index.html
-cat templates/top.html content/services_content.html templates/bottom.html > docs/services.html
-cat templates/top.html content/about_content.html templates/bottom.html > docs/about.html
-cat templates/top.html content/contact_content.html templates/bottom.html > docs/contact.html
