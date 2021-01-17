374812d947207dae004cc5bdbb27510fb30c7fb8
Remy Johnson <remyvjohnson@gmail.com>
2021-01-16 22:42:06 -0800

    first upload from HW4

--- build.py
+++ build.py
@@ -1,29 +1,67 @@
 print("Yep, it's working")
 
-content_pages = [
-    {
-        'input_filename': 'content/index_content.html',
-        'output_filename': 'docs/index.html',
-        'title': 'RVJ. Retail Consulting: Home',
-    },
-    {
-        'input_filename': 'content/services_content.html',
-        'output_filename': 'docs/services.html',
-        'title': 'RVJ. Retail Consulting: Services',
-    },
-    {
-        'input_filename': 'content/about_content.html',
-        'output_filename': 'docs/about.html',
-        'title': 'RVJ. Retail Consulting: About Me',
-    },
-    {
-        'input_filename': 'content/contact_content.html',
-        'output_filename': 'docs/contact.html',
-        'title': 'RVJ. Retail Consulting: Contact Me',
-    },
-]
+# content_pages = [
+#     {
+#         'input_filename': 'content/index_content.html',
+#         'output_filename': 'docs/index.html',
+#         'title': 'RVJ. Retail Consulting: Home',
+#     },
+#     {
+#         'input_filename': 'content/services_content.html',
+#         'output_filename': 'docs/services.html',
+#         'title': 'RVJ. Retail Consulting: Services',
+#     },
+#     {
+#         'input_filename': 'content/about_content.html',
+#         'output_filename': 'docs/about.html',
+#         'title': 'RVJ. Retail Consulting: About Me',
+#     },
+#     {
+#         'input_filename': 'content/contact_content.html',
+#         'output_filename': 'docs/contact.html',
+#         'title': 'RVJ. Retail Consulting: Contact Me',
+#     },
+# ]
 
-#Template Function - pulls in base.html and updates the title
+import glob
+import os
+
+#loop through content files and create a list
+content_files = []
+for content_file in glob.glob("content/*.html"): 
+    content_files.append(content_file)
+    # print(content_files)
+    print('-PT1 COMPLETE-')
+
+    #extract info from file path names
+    all_files = content_files
+    i = 0
+    content_pages = []
+    for all_file in all_files:  
+        file_path = all_files[i]
+        file_name = os.path.basename(file_path)
+        print(file_name)
+        name_only, extension = os.path.splitext(file_name)
+        print(name_only)
+        print('-PT2 COMPLETE-')
+        i += 1
+
+
+        
+        # for content_file in content_files:
+        content_pages.append({
+        "input_filename": "content/"+ file_name,
+        "title": name_only,
+        "output_filename": "docs/"+ file_name,
+        })
+        print(content_pages)
+        print('-THE END!-')
+    
+
+
+
+
+# Template Function - pulls in base.html and updates the title
 def templates(content_page):
     template = open('templates/base.html').read()
     updated_title_template = template.replace("{{title}}", content_page['title'])
